875a6b0e4bf6675dadf2e9e47f61d48dbff86170
Don_martinez.eth <donmartinez@aluno.unilab.edu.br>
2026-04-16 02:54:36 -0300

    chore

--- martinez/next.config.ts
+++ martinez/next.config.ts
@@ -3,5 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
+  basePath: '/martinez',
+  images: {
+    unoptimized: true,
+  }
 };
 module.exports = nextConfig;
