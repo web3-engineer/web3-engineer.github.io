7172a65758e80a93bbfdeb98f6a8695c121fcc2d
Don_martinez.eth <donmartinez@aluno.unilab.edu.br>
2026-04-16 03:05:06 -0300

    lets go

--- martinez/next.config.ts
+++ martinez/next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  /* config options here */
+  output: 'export',
+  // O basePath deve ser exatamente o nome do seu repositório no GitHub
   basePath: '/martinez',
+  // Garante que links como /vitrine virem /vitrine/ (melhor para o GitHub Pages)
+  trailingSlash: true,
   images: {
     unoptimized: true,
-  }
+  },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
